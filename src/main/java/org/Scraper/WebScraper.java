1049f35de99c972e680c1a5677b4cbf2e959d5a1
Jason Cameron <dev.jasoncameron@gmail.com>
2023-10-26 06:09:47 -0500

    built first page scraper ;

---
+++ src/main/java/org/Scraper/WebScraper.java
@@ -0,0 +1,47 @@
+package org.Scraper;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+public class WebScraper {
+
+    private static final String PURL = "https://www.paranormaldatabase.com/index.html";
+
+    public static void main(String[] args) throws IOException {
+
+        InputStream stream = null;
+        BufferedInputStream buf = null;
+
+        try {
+            URL url = new URL(PURL);
+            stream = url.openStream();
+            buf = new BufferedInputStream(stream);
+
+            StringBuilder sb = new StringBuilder();
+
+            while (true) {
+                int data = buf.read();
+
+                if (data == -1) {
+                    break;
+                } else {
+                    sb.append((char) data);
+                }
+            }
+
+            System.out.println(sb);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            stream.close();
+            buf.close();
+            System.out.println("Completed Run");
+        }
+
+    }
+
+}
