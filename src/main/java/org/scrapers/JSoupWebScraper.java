4155fe4a097f3e416d1ea70f30cfd454cc6df34e
Jason Cameron <dev.jasoncameron@gmail.com>
2023-10-29 20:18:33 -0500

    jsoup with signif delay can work... if too short, it breaks

--- src/main/java/org/scrapers/JSoupWebScraper.java
+++ src/main/java/org/scrapers/JSoupWebScraper.java
@@ -2,6 +2,8 @@ package org.scrapers;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
@@ -9,11 +11,20 @@ public class JSoupWebScraper {
 
     public static void main(String[] args) {
 
-        try {
-            String url = "https://www.imdb.com/";
-            Document doc = Jsoup.connect(url).get();
+        String url = "https://www.imdb.com/chart/top/";
 
-            System.out.println(doc);
+
+
+        try {
+
+            Document doc = Jsoup.connect(url).timeout(6000).get();
+            Elements body = doc.select("ul.ipc-metadata-list");
+//            System.out.println(body.select("li").size());
+
+            for(Element e : body.select("li")){
+                String title = e.select("div.ipc-metadata-list-summary-item__c").text();
+                System.out.println(title);
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
